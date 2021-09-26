302043cea64caef448b1f70baa779f74230acd22
alu <alu@VDI-Ubuntu-EPS-2019>
2021-09-26 23:10:39 +0200

    añadir

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -1,5 +1,15 @@
 package model;
 
 public class Coordinate {
-
+	private int x,y;
+	public{
+		Coordinate(int x, int y);
+		Coordinate(final Coordinate&);
+		int getX() final { return x; }
+		int getY() final { return y; }
+		bool operator equals(final Coordinate&) final;
+		string to_string() final;
+		Coordinate add(final Coordinate& c) final;
+		Coordinate add(int x,int y) final;
+	}
 }

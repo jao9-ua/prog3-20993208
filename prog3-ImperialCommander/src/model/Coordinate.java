ee27b0a939fa358856209440d12255c75eb69cbe
jao9 <jao9@alu.ua.es>
2021-10-21 21:48:01 +0200

    añadir

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -2,19 +2,56 @@ package model;
 
 public class Coordinate {
 	private int x,y;
-	public Coordinate(int x, int y) {}
-	public Coordinate( Coordinate coordinate );
+	
+	public Coordinate(int x, int y) {
+		this.y=y;
+		this.x=x;
+	}
+	public Coordinate( Coordinate c ) {
+		x=c.x;
+		y=c.y;
+		
+	}
+	
 	public int getX() {
 		return x;
 		}
+	
 	public int getY() {
 		return y;
 		}
-	public bool operator.equals( Coordinate coordinate);
-	public  String toString();
-	public Coordinate add( Coordinate c) {
-		return null;
+	
+	public boolean equals( Coordinate c){
+		Boolean a = null;
+		if(c==this) {
+			a= true;
+		}
+		if(c == null) {
+			a= false;
+		}
+		if(!(c instanceof Coordinate)) {
+			a= false;
+		}
+		if(x==c.x && y==c.y) {
+			a= true;
+		}
+		return a;
 	}
-	public Coordinate add(int x,int y) ;
+	
+	public  String toString() {
+		StringBuilder concatenation=new StringBuilder();
+		concatenation.append( "["+ x + "," + y + "]");
+		return concatenation.toString();
+	}
+	
+	public Coordinate add( Coordinate c) {
+		Coordinate new_c= new Coordinate(x+c.x,y+c.y);
+		return new_c;
+		
+	}
+	public Coordinate add(int x,int y) {
+		Coordinate new_c= new Coordinate(this.x+x,this.y+y);
+		return new_c;
+	}
 
 }
